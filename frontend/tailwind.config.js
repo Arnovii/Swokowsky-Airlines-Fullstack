3bc922a777e316c16012bdbf931736d7d83ceb99
valen725 <juana.martinez@utp.edu.co>
2025-09-06 16:53:32 -0500

    feat: Tailwind e iniciacion de carpetas frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          black: "#000",
+          white: "#fff",
+          darkblue: "#123361",
+          cyan: "#39A5D8",
+          darkcyan: "#1180B8",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
