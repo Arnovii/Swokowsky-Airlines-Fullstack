76b0ec7be0917c83a347c4c8dc82346284905911
VRDARK33 <brayan54sd@gmail.com>
2025-09-14 10:42:56 -0500

    integracion de la base de datos en compose y creacion de archivo env.example

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'prueba de backend';
+    return 'prueba de backend esto lotoy po desde el frontend';
   }
 }
--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get} from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Get()
+    getAllAuth() {
+        return this.authService.getAllAuth();
+    }
+
+
+
+
+}
