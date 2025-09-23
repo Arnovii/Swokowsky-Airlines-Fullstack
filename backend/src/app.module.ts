e6f20333a0b3c6f8425c5c8d90fa7b6be6f73ab1
Arnovii <arnovi.jimenez@utp.edu.co>
2025-09-23 17:01:08 -0500

    Database design changed and basic seed.ts created in prisma folder

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { FlightsModule } from './modules/flights/flights.module';
 import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
@@ -18,7 +17,6 @@ import { MailModule } from './mail/mail.module';
     }),
     DatabaseModule,
     AuthModule,
-    FlightsModule,
     UsersModule,
     ProfileModule,
     MailModule],
