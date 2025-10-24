b5ec6b3899eddef67e18a687eb4f3ecb2667d402
lvinascovj <luis.vinasco@utp.edu.co>
2025-10-24 12:28:53 -0500

    carrito y monedero, correjir flujo

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,21 +1,21 @@
-import { Routes, Route } from "react-router-dom"
-import MainLayout from "../layouts/MainLayout"
-import Home from "../modules/home/pages/Home"
-import Perfil from "../modules/user_profile/pages/Perfil"
-import Login from "../modules/auth/pages/Login"
-import Register from "../modules/auth/pages/Register"
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../modules/home/pages/Home";
+import Perfil from "../modules/user_profile/pages/Perfil";
+import Login from "../modules/auth/pages/Login";
+import Register from "../modules/auth/pages/Register";
 import ResetPassword from "../modules/auth/pages/ResetPassword";
-import ForgotPassword from "../modules/auth/pages/ForgotPassword"
-import NotFound from "../modules/error/pages/NotFound"
+import ForgotPassword from "../modules/auth/pages/ForgotPassword";
+import NotFound from "../modules/error/pages/NotFound";
 import { FlightSearchResults } from "../modules/flightsearch/pages/FlightSearchResults";
 import FlightDetailsPage from "../modules/flightsearch/pages/FlightDetailsPage";
-import News from "../modules/news/pages/News"
-import DetalleOferta from "../modules/news/pages/detalleoferta"
-import DetalleVuelo from "../modules/news/pages/detallevuelo"
-import PanelAdministrador from "../modules/panelAdministrador/pages/PanelAdministrador"
-import PrivateRoute from "./PrivateRoute"
-import AdminRoute from "./AdminRoute"
-
+import News from "../modules/news/pages/News";
+import DetalleOferta from "../modules/news/pages/detalleoferta";
+import DetalleVuelo from "../modules/news/pages/detallevuelo";
+import PanelAdministrador from "../modules/panelAdministrador/pages/PanelAdministrador";
+import PrivateRoute from "./PrivateRoute";
+import AdminRoute from "./AdminRoute";
+import Carrito from "../modules/home/pages/Carrito";
 
 export default function AppRoutes() {
   return (
@@ -23,31 +23,48 @@ export default function AppRoutes() {
       {/* Rutas principales con header/footer */}
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/perfil"
+        
+        <Route
+          path="/perfil"
           element={
             <PrivateRoute>
               <Perfil />
             </PrivateRoute>
-          } />
+          }
+        />
+
+        {/*Ruta protegida del carrito */}
+        <Route
+          path="/carrito"
+          element={
+            <PrivateRoute>
+              <Carrito />
+            </PrivateRoute>
+          }
+        />
+
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/buscar-vuelos" element={<FlightSearchResults />} />
-    <Route path="/detalle-vuelo/:id" element={<FlightDetailsPage />} />
+        <Route path="/detalle-vuelo/:id" element={<FlightDetailsPage />} />
         <Route path="/news" element={<News />} />
         <Route path="/noticias/oferta/:id" element={<DetalleOferta />} />
         <Route path="/noticias/vuelo/:id" element={<DetalleVuelo />} />
-        <Route path="/panelAdministrador"
+
+        <Route
+          path="/panelAdministrador"
           element={
             <AdminRoute>
               <PanelAdministrador />
             </AdminRoute>
-          } />
+          }
+        />
       </Route>
 
       {/* Página 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
-  )
+  );
 }
