7913492eb38ff8167c806d28a0ae98c0ad28d224
Arnovii <arnovi.jimenez@utp.edu.co>
2025-12-07 03:39:26 -0500

    Fixing some frontend bugs and DB deployed and configurated with backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin:  'http://localhost:5173',
+    origin:  ['http://localhost:5173', 'https://swokowsky-airlines.infinityfree.me'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
