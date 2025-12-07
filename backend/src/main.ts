5670c000ec0abef2fa436061274d513aee437721
Arnovii <arnovi.jimenez@utp.edu.co>
2025-12-07 04:01:30 -0500

    fixing port bug in render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -58,6 +58,7 @@ async function bootstrap() {
     console.log('Swagger disponible en: /api/docs');
 
 
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
 } 
 bootstrap();
