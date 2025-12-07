10aa604fef74ed4938dd65293df9a20fe8019aa5
Arnovii <arnovi.jimenez@utp.edu.co>
2025-12-07 04:41:41 -0500

    final deploy render corrections

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -58,7 +58,8 @@ async function bootstrap() {
     console.log('Swagger disponible en: /api/docs');
 
 
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(PORT);
+  console.log(`🚀Server running on port ${PORT}`);
 } 
 bootstrap();
